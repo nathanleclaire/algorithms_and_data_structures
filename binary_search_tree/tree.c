1a8502e88416b12fdf55c5c1f9c93bec907397ee
Nathan LeClaire <nathan.leclaire@gmail.com>
2013-07-06 23:58:31 -0400

    BST

---
+++ binary_search_tree/tree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct Node {
+    struct Node* leftChild;
+    struct Node* rightChild;
+    int val;
+};
+ 
+struct Node* createTree(int[] nums, int size) {
+	
+}
+
+int main() {
+    printf("hello world\n");
+    return 0;
+}
