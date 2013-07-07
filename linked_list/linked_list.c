6cfbafab082a136bbdb6d2d0bfdefaa0a2b04b59
Nathan LeClaire <nathan.leclaire@gmail.com>
2013-07-06 22:25:40 -0400

    Ongoing camelcase to snake_case project

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -26,27 +26,27 @@ void append(struct linked_list* node, int val) {
 	}
 }
 
-void printList(struct linked_list* l) {
+void print_list(struct linked_list* l) {
 	if ( l->next == NULL ) {
 		printf("(%d)\n", l->val);
 	} else {
 		printf("(%d) -> ", l->val);
-		printList(l->next);
+		print_list(l->next);
 	}
 }
 
-void dumpNodes(struct linked_list* l) {
+void dump_nodes(struct linked_list* l) {
 	if ( l->next == NULL ) {
 		printf("\n");
 	} else {
 		printf("(%d): %p\n", l->val, l->next);
-		dumpNodes(l->next);
+		dump_nodes(l->next);
 	}
 }
 
 int main(int argc, char* argv[]) {
 
-	struct linked_list* myLL;
+	struct linked_list* my_ll;
 	int i;
 
 	if ( argc > 1 ) {
@@ -54,18 +54,18 @@ int main(int argc, char* argv[]) {
 		// allocate the first node in the linked list
 		// use the first command line arg for the value
 	
-		myLL = alloc_node(NULL, atoi(argv[1])); 	
+		my_ll = alloc_node(NULL, atoi(argv[1])); 	
 
 		// start appending at the second command line argument
 
 		for(i=2; i < argc; i++) {
-			append(myLL, atoi(argv[i]));
-			printList(myLL);
+			append(my_ll, atoi(argv[i]));
+			print_list(my_ll);
 			printf("\n");
-			dumpNodes(myLL);
+			dump_nodes(my_ll);
 		}
 
-		printList(myLL);
+		print_list(my_ll);
 
 	} else {
 		printf("Dude, no arguments?  Not cool.\n");
